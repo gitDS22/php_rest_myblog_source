11a16bed884153b12a5016c2e4c8a758fd669a97
David <d_schiffer@mail.fhsu.edu>
2022-03-03 09:31:05 -0600

    third

--- config/Database.php
+++ config/Database.php
@@ -1,25 +1,29 @@
 <?php
     class Database {
         //DB params
-        private $host = 'localhost';
-        private $db_name = 'myblog';
-        private $username = 'root';
-        private $password = '';
-        private $conn;
+        $url = getenv('JAWSDB_URL');
+        $dbparts = parse_url($url);
+
+        $hostname = $dbparts['host'];
+        $username = $dbparts['user'];
+        $password = $dbparts['pass'];
+        $database = ltrim($dbparts['path'],'/');
+
 
         //DB connect
         public function connect() {
-            $this->conn = null;
-
+            //$this->conn = null;
             try {
-                $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name,
-                $this->username,$this->password);
-                $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            } catch(PDOException $e) {
-                echo 'Connection Error: ' . $e->getMessage();
-            }
-            return $this->conn;
-        }
+                $conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
+                // set the PDO error mode to exception
+                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                echo "Connected successfully";
+                }
+            catch(PDOException $e)
+                {
+                echo "Connection failed: " . $e->getMessage();
+                }
+
     }
 
     ?>
